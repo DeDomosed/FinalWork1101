9b19f19ae07cdf7fd8e2aa70c827ae824a37bbc0
DeDomosed <poperm137@gmail.com>
2024-12-20 03:53:55 +0300

    Add files via upload

---
+++ StoreLibrary/Models/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace StoreLibrary.Models;
+
+public partial class Order
+{
+    public int OrderId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public int PickupPointId { get; set; }
+
+    public string OrderStatus { get; set; } = null!;
+
+    public DateTime? OrderDeliveryDate { get; set; }
+
+    public DateTime OrderDate { get; set; }
+
+    public short OrderPickupCode { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<OrderProduct> OrderProducts { get; set; } = new List<OrderProduct>();
+
+    [JsonIgnore]
+    public virtual PickupPoint? PickupPoint { get; set; } = null!;
+    [JsonIgnore]
+    public virtual User? User { get; set; } = null!;
+}
