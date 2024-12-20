c39a41f7f521500591260946c11e64c97be9474d
DeDomosed <poperm137@gmail.com>
2024-12-20 04:05:29 +0300

    Add files via upload

---
+++ Desktop/Pages/ShopPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Desktop.Pages;
+using StoreLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Store.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ShopPage.xaml
+    /// </summary>
+    public partial class ShopPage : Page
+    {
+        public ShopPage()
+        {
+            InitializeComponent();
+            var currentUser = Application.Current.Properties["CurrentUser"] as User;
+            UpdateLoginButton(currentUser);
+
+            if (currentUser != null)
+            {
+                FullNameTextBlock.Text = $"{currentUser.UserSurname} {currentUser.UserName} {currentUser.UserPatronymic}";
+
+                if (currentUser.RoleId == 2 || currentUser.RoleId == 3)
+                    OrderPanelButton.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                FullNameTextBlock.Text = "Гость";
+                OrderPanelButton.Visibility = Visibility.Collapsed;
+            }
+
+            
+        }
+        private void UpdateLoginButton(User? currentUser)
+        {
+            if (currentUser == null)
+                LoginButton.Content = "Войти";
+            else
+                LoginButton.Content = "Выйти";
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var currentUser = Application.Current.Properties["CurrentUser"] as User;
+
+            if (currentUser == null)
+                NavigationService.Navigate(new AuthorizationPage());
+            else
+                Application.Current.Properties["CurrentUser"] = null;
+                NavigationService.Navigate(new AuthorizationPage());
+        }
+
+        private void OrderPanelButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new OrderPanelPage());
+        }
+    }
+}
